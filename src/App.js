9b0c97837d9a24ea5de70fff2b1f558834bf5985
Nathnael Yirga <natty@localhost.localdomain>
2024-07-03 22:16:49 +0300

    remove tele console.logs for android views

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 // import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 import Landing from "./components/Landing";
 import SomeProfs from "./components/SomeProfs";
-import AddNBrowse from "./components/AddNBrowse"
-
+import AddNBrowse from "./components/AddNBrowse";
 
 function App() {
-  console.log(window.Telegram.WebApp)
-
-  console.log(window.Telegram.WebApp.colorScheme)
-  return ( <div className = "App container d-flex flex-column justify-content-around  align-items-center" >
-    <Landing / >
-    <AddNBrowse />
+  return (
+    <div className="App container d-flex flex-column justify-content-around  align-items-center">
+      <Landing />
+      <AddNBrowse />
     </div>
   );
 }
