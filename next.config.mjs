527e957edf51c183f291458dddb253c05fb029df
Nathnael Yirga <natty@localhost.localdomain>
2024-07-07 01:40:30 +0300

    < />

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    distDir: '.next', // Specifies the output directory for the build files
+};
 
 export default nextConfig;
