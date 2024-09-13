31ef817c63a3a4606cb94c8680ed411ad500f9ec
Nathnael Yirga <Nathnaelyirga@gmail.com>
2024-09-13 13:10:37 +0300

    nav-bar css

--- src/app/_components/Nav.tsx
+++ src/app/_components/Nav.tsx
@@ -1,13 +1,44 @@
+
+import { useState } from 'react';
 import Link from 'next/link';
+import styles from './NavBar.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faSearch, faPlus } from '@fortawesome/free-solid-svg-icons';
 
-function Nav() {
+export default function Nav() {
+  const [selected, setSelected] = useState<'home' | 'browse' | 'add'>('home');
+
   return (
-    <div className="d-flex justify-content-evenly ">
-      <Link href="/">home</Link>
-      <Link href="/browse">Browse</Link>
-      <Link href="/addEditProf/new">Add</Link>
-    </div>
-  )
-}
+    <nav className={styles.navBar}>
+      <Link href="/" passHref>
+        <div
+          className={`${styles.navItem} ${selected === 'home' ? styles.active : ''}`}
+          onClick={() => setSelected('home')}
+        >
+          <FontAwesomeIcon icon={faHome} className={styles.icon} />
+          {selected === 'home' && <span>Home</span>}
+        </div>
+      </Link>
 
-export default Nav
+      <Link href="/browse" passHref>
+        <div
+          className={`${styles.navItem} ${selected === 'browse' ? styles.active : ''}`}
+          onClick={() => setSelected('browse')}
+        >
+          <FontAwesomeIcon icon={faSearch} className={styles.icon} />
+          {selected === 'browse' && <span>Browse</span>}
+        </div>
+      </Link>
+
+      <Link href="/addEditProf/new" passHref>
+        <div
+          className={`${styles.navItem} ${selected === 'add' ? styles.active : ''}`}
+          onClick={() => setSelected('add')}
+        >
+          <FontAwesomeIcon icon={faPlus} className={styles.icon} />
+          {selected === 'add' && <span>Add</span>}
+        </div>
+      </Link>
+    </nav>
+  );
+}
